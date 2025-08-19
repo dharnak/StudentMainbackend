f6ae62170584254b0f6a28845240772ca7b1764b
dharnak <thangavelraja36@gmail.com>
2025-08-19 17:54:03 +0530

    fix: setup build for cloud database connection

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,23 @@
-import { Sequelize } from 'sequelize-typescript';
-import { Student } from './models/Student';
-import { Department } from './models/Department';
+import { Sequelize } from "sequelize-typescript";
+import { Student } from "./models/Student";
+import { Department } from "./models/Department";
 
-const sequelize = new Sequelize('demopost', 'postgres', 'Dharn@123', {
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
+const connectionString =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:Dharn@123@localhost:5432/demopost"; 
+
+const sequelize = new Sequelize(connectionString, {
+  dialect: "postgres",
+  dialectOptions: process.env.DATABASE_URL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
   logging: false,
+  models: [Student, Department],
 });
-
-sequelize.addModels([Student, Department]);
 
 export default sequelize;
